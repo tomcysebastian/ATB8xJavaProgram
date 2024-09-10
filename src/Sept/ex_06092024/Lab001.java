75f14486a9f58e8bf83794c1637b1ee0298637ac
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-10 18:25:49 +0530

    Push this code to github

--- src/Sept/ex_06092024/Lab001.java
+++
@@ -1,4 +0,0 @@
-package Sept.ex_06092024;
-
-public class Lab001 {
-}
---
+++ src/Sept/ex_06092024/Lab004.java
@@ -0,0 +1,17 @@
+package Sept.ex_06092024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(2+2);
+        System.out.println(4+9);
+        //testing this one
+        /*
+        *
+        * testing this one
+        * ready to push to git hub
+        *
+        */
+    }
+}
