1f5c0f8f061f508f6e47a559009d0975741a744d
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-25 13:50:49 +0530

    Push this code to github

--- src/Sept/ex_25092024/Task20.java
+++ src/Sept/ex_25092024/Task20.java
@@ -1,9 +1,13 @@
 package Sept.ex_25092024;
+//Factorial Program - n = 5 , Output = 120 //  n = 5 ->  Fact = 5*4*3*2*1 = 120
+// n = 1 ->  Fact = 1*1 -> 1
+// n = 3 -> Fact = 3*2*1 -> 6
 
 public class Task20 {
 
     public static void main(String[] args)
     {
+
         int no=5,fact=1;
 
         //int factorial = 0;
