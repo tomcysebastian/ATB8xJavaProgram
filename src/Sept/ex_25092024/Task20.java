298c02adf62f5ff82c52774e2603dddd983830ee
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-25 13:48:38 +0530

    Push this code to github

---
+++ src/Sept/ex_25092024/Task20.java
@@ -0,0 +1,16 @@
+package Sept.ex_25092024;
+
+public class Task20 {
+
+    public static void main(String[] args)
+    {
+        int no=5,fact=1;
+
+        //int factorial = 0;
+        for(int i=1;i<=no;i++)
+        {
+           fact=fact*i;
+        }
+        System.out.println("factorial of 5"+"="+fact);
+    }
+}
