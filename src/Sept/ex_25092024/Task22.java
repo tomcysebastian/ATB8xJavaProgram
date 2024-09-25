b9ab874db5072a31c8e9913f9013243892304217
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-25 14:47:00 +0530

    Push this code to github

---
+++ src/Sept/ex_25092024/Task22.java
@@ -0,0 +1,31 @@
+package Sept.ex_25092024;
+
+public class Task22 {
+
+    public static void main(String[] args)
+    {
+        String s="TOMCY SEBASTIAN";
+        int vowels=0, consonants=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+            {
+                vowels++;
+            }
+            else if(ch == ' ')
+            {
+                continue;
+            }
+            else
+            {
+                consonants++;
+            }
+
+        }
+        System.out.println("vowels"+vowels);
+        System.out.println("consonants"+consonants);
+    }
+
+}
