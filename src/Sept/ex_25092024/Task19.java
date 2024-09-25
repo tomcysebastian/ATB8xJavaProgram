2327cc4eccb98ab25c1d5d7b8504c170b12d2700
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-25 13:01:49 +0530

    Push this code to github

---
+++ src/Sept/ex_25092024/Task19.java
@@ -0,0 +1,15 @@
+package Sept.ex_25092024;
+
+public class Task19 {
+  public static void main(String[] args){
+
+      //10*1=10
+      int n=10;
+
+      for(int i=1;i<=10;i++)
+      {
+          System.out.println(n+"*"+i+"="+(n*i));
+
+      }
+  }
+}
