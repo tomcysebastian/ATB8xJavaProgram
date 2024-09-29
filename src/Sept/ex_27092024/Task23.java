28ef26813734a14595e46cf7c5f7c7bc343ce388
TomcySebastian <tomcysebastian7@gmail.com>
2024-09-29 21:17:49 +0530

    Push this code to github

---
+++ src/Sept/ex_27092024/Task23.java
@@ -0,0 +1,21 @@
+package Sept.ex_27092024;
+
+public class Task23 {
+    public static void main(String[] args)
+    {
+        int i=1;
+        while (i<1)
+        {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("while loop=>1 is not less than 1 , its equal to 1");
+
+        do
+        {
+            System.out.println("do while loop, value is =>"+i);
+            i++;
+        }while(i<1);
+
+    }
+}
